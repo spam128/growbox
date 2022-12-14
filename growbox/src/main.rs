a6cdc3738c597ce30039134939fcb723ff1813a9
corpuscallosum <dlesniak91@gmail.com>
2022-12-14 02:16:13 +0100

    added lcd

--- growbox/src/main.rs
+++ growbox/src/main.rs
@@ -5,6 +5,8 @@
 use cortex_m_rt::entry; // The runtime
 use embedded_hal::digital::v2::OutputPin; // the `set_high/low`function
 use stm32f1xx_hal::{delay::Delay, pac, prelude::*}; // STM32F1 specific functions
+use lcd1602_rs::LCD1602; // lcd https://crates.io/crates/lcd1602-rs
+
 #[allow(unused_imports)]
 use panic_halt; // When a panic occurs, stop the microcontroller
 
@@ -45,10 +47,54 @@ fn main() -> ! {
     // other function can access it. Otherwise the timer could be reset during a
     // delay.
     let mut delay = Delay::new(cp.SYST, clocks);
+    //===========================
+    // https://github.com/levpaul/lcd1602-rs/blob/92b348f76fd1d8611527d5fb21580e2bb7230dba/examples/teensy40/src/main.rs
+    // LCD controller
+    let mut p = bsp::Peripherals::take().unwrap();
+    let pins = bsp::t40::into_pins(p.iomuxc);
+    let mut led = bsp::configure_led(pins.p13);
+
+    // Init pins
+    let rs = gpio::GPIO::new(pins.p12).output();
+    let en = gpio::GPIO::new(pins.p11).output();
+    let d4 = gpio::GPIO::new(pins.p5).output();
+    let d5 = gpio::GPIO::new(pins.p4).output();
+    let d6 = gpio::GPIO::new(pins.p3).output();
+    let d7 = gpio::GPIO::new(pins.p2).output();
+
+    // General Purpose Timer setup
+    let (_, ipg_hz) =
+        p.ccm
+            .pll1
+            .set_arm_clock(imxrt_hal::ccm::PLL1::ARM_HZ, &mut p.ccm.handle, &mut p.dcdc);
+    let mut cfg = p.ccm.perclk.configure(
+        &mut p.ccm.handle,
+        imxrt_hal::ccm::perclk::PODF::DIVIDE_3,
+        imxrt_hal::ccm::perclk::CLKSEL::IPG(ipg_hz),
+    );
+    let mut gpt1 = p.gpt1.clock(&mut cfg);
+    gpt1.set_output_interrupt_on_compare(imxrt_hal::gpt::OutputCompareRegister::Three, false);
+    gpt1.set_mode(imxrt_hal::gpt::Mode::FreeRunning);
+    gpt1.set_reset_on_enable(true);
+    gpt1.set_enable(true);
+    let t = gpt1.count_down(imxrt_hal::gpt::OutputCompareRegister::Three);
+
+    // LCD Init
+    let mut lcd = LCD1602::new(en, rs, d4, d5, d6, d7, t).unwrap();
+    // =================================
+
 
     // Now, enjoy the lightshow!
     loop {
-        led.set_high().ok();
+        // lcd display
+        lcd.print("hello world!").ok();
+        led.toggle();
+        lcd.delay(1_000_000 as u64).ok();
+        led.toggle();
+        lcd.clear().ok();
+        lcd.delay(1_000_000 as u64).ok();
+
+        // led on micro
         delay.delay_ms(1_000_u16);
         led.set_low().ok();
         delay.delay_ms(1_000_u16);
