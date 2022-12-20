b6071ad852cf5e117a4203de7d80caef3a144ff6
corpuscallosum <dlesniak91@gmail.com>
2022-12-20 02:24:58 +0100

    add heath variance to avoid blinking heater

--- growbox/src/main.rs
+++ growbox/src/main.rs
@@ -24,6 +24,7 @@ use dht11::Dht11;
 use panic_halt; // When a panic occurs, stop the microcontroller
 
 const HEAT_TEMP: i16 = 230;
+const HEAT_TEMP_VARIANCE: i16 = 5;
 
 // This marks the entrypoint of our application. The cortex_m_rt creates some
 // startup code before this, but we don't need to worry about this
@@ -54,7 +55,7 @@ fn main() -> ! {
     // This gives us an exclusive handle to the GPIOC peripheral. To get the
     // handle to a single pin, we need to configure the pin first. Pin C13
     // is usually connected to the Bluepills onboard LED.
-    let mut heath_pin = gpioc.pc13.into_open_drain_output(&mut gpioc.crh);
+    let mut heath_pin = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
     let dht11_pin = gpioc.pc14.into_open_drain_output(&mut gpioc.crh);
     let mut dht11_error_pin = gpioc.pc15.into_push_pull_output(&mut gpioc.crh);
 
@@ -91,13 +92,13 @@ fn main() -> ! {
 }
 
 fn control_heat(measurement: dht11::Measurement,
-                heath_pin: &mut gpio::gpioc::PC13<gpio::Output<gpio::OpenDrain>>,
+                heath_pin: &mut gpio::gpioc::PC13<gpio::Output<gpio::PushPull>>,
                 dht11_error_pin: &mut gpio::gpioc::PC15<gpio::Output<gpio::PushPull>>) {
     // The measured temperature is in tenths of degrees Celsius.
-    if (measurement.temperature) < HEAT_TEMP {
+    if (measurement.temperature) < HEAT_TEMP - HEAT_TEMP_VARIANCE {
         // heat on
         heath_pin.set_high().ok();
-    } else {
+    } else if (measurement.temperature) > HEAT_TEMP + HEAT_TEMP_VARIANCE {
         // heat off
         heath_pin.set_low().ok();
     }
